c8ec51385e2ab1140cf78288704a39935069a8b8
SteveGuoh <hghandle@outlook.com>
2023-10-01 16:58:40 +0800

    First commit, already implement login/out, checkout, showreview and read books from db

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Route, Switch, Redirect, useHistory } from "react-router-dom";
+import { Homepage } from "./layouts/Homepage/HomePage";
+import { Footer } from "./layouts/NavbarAndFooter/Footer";
+import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
+import { SearchBooksPage } from "./layouts/SearchBooksPage/SearchBooksPage";
+import { BookCheckoutPage } from "./layouts/BookCheckoutPage/BookCheckoutPage";
+import { oktaConfig } from "./lib/oktaConfig";
+import { OktaAuth, toRelativeUrl } from "@okta/okta-auth-js";
+import { LoginCallback, Security } from "@okta/okta-react";
+import LoginWidget from "./Auth/LoginWidget";
 
-function App() {
+const oktAuth = new OktaAuth(oktaConfig);
+
+export const App = () => {
+  const customerAuthHandler = () => {
+    // eslint-disable-next-line
+    history.push("/login");
+  };
+
+  const history = useHistory();
+
+  const restoreOriginalUri = async (_otkaAuth: any, originalUri: any) => {
+    history.replace(toRelativeUrl(originalUri || "/", window.location.origin));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="d-flex flex-column min-vh-100">
+      <Security
+        oktaAuth={oktAuth}
+        restoreOriginalUri={restoreOriginalUri}
+        onAuthRequired={customerAuthHandler}
+      >
+        <Navbar />
+        <div className="flex-grow-1">
+          <Switch>
+            <Route path="/" exact>
+              <Redirect to="/home" />
+            </Route>
+            <Route path="/home">
+              <Homepage />
+            </Route>
+            <Route path="/search">
+              <SearchBooksPage />
+            </Route>
+
+            <Route path="/checkout/:bookId">
+              <BookCheckoutPage />
+            </Route>
+            <Route
+              path="/login"
+              render={() => <LoginWidget config={oktaConfig} />}
+            />
+            <Route path="/login/callback" component={LoginCallback} />
+          </Switch>
+        </div>
+        <Footer />
+      </Security>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/lib/oktaConfig.ts
@@ -0,0 +1,8 @@
+export const oktaConfig = {
+    clientId: "0oabmvp86t6q9DG8q5d7",
+    issuer: "https://dev-85965405.okta.com/oauth2/default",
+    redirectUri: "http://localhost:3000/login/callback",
+    scopes: ["openid", "profile", "email"],
+    pkce: true,
+    disableHttpsCheck: true,
+}
